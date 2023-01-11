e79c29bfe18b3adaea082dc8fd988b00c5583195
Adam Podkowinski <adampodkdev@gmail.com>
2023-01-12 00:32:10 +0100

    Add prisma

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -1,6 +1,7 @@
 import Auth0Provider from "next-auth/providers/auth0";
 import { NuxtAuthHandler } from "#auth";
 
+// TODO: move from auth0 to supabase
 export default NuxtAuthHandler({
   providers: [
     // @ts-expect-error .default to work during SSR
--- server/api/auth/protected.ts
+++ server/api/auth/protected.ts
@@ -1,7 +1,15 @@
+import { PrismaClient } from "@prisma/client";
 import { getServerSession, getToken } from "#auth";
+
+const prisma = new PrismaClient();
 
 export default defineEventHandler(async (event) => {
   const session = await getServerSession(event);
   const token = await getToken({ event });
-  return { email: session?.user?.email, user_id: token?.sub };
+  const book = await prisma.books.findFirst();
+  return {
+    email: session?.user?.email,
+    user_id: token?.sub,
+    firstBook: book?.library_id,
+  };
 });
